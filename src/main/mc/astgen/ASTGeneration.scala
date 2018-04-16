0a0b9f7e7ee78a05d6f87073ed608da7f327425e
Unknown <thanh.hoquangcse96@gmail.com>
2018-04-16 08:52:21 +0700

    AST gen ver 0.1

--- src/main/mc/astgen/ASTGeneration.scala
+++ src/main/mc/astgen/ASTGeneration.scala
@@ -12,9 +12,30 @@ import mc.parser.MCParser._
 
 class ASTGeneration extends MCBaseVisitor[Any] {
 
+  //Using to flat the List
+  def flatten(lst: List[Any]): List[Any] = lst flatMap {
+    case i: List[_] => flatten(i)
+    case e => List(e)
+  }
+
   override def visitProgram(ctx:ProgramContext) = 
-  			Program(List(FuncDecl(Id("main"),List(),VoidType,Block(List(),List()))))
+  			Program(ctx.declList.asInstanceOf[List[Decl]])
 
-	
+  override def visitDeclList(ctx: DeclListContext) = 
+        flatten(ctx.decl.asScala.toList.map(_.accept(this)))
 
+  override def visitDecl(ctx: DeclContext) =
+        ctx.getChild(0).accept(this)
+
+  override def visitPrimitiveType(ctx: PrimitiveTypeContext) = 
+        if(ctx.BOOLTYPE) BoolType()
+        else if(ctx.FLOATTYPE) FloatType 
+        else if(ctx.INTTYPE) IntType 
+        else StringType
+
+  override def visitVarDecl(ctx: VarDeclContext) = {
+        var type = ctx.primitiveType.accept(this).asInstanceOf[Type]
+        ctx.manyVar.asScala.toList.map(_.accept(this))
+  }
+  
 }
--- src/test/scala/AstSuite.scala
+++ src/test/scala/AstSuite.scala
@@ -5,19 +5,26 @@ import mc.utils._
   * Created by nhphung on 4/29/17.
   */
 class AstSuite extends FunSuite with TestAst {
-test("a simple program with void as return type of main") {
-  val input = "void main () {}"
-  val expected = Program(List(FuncDecl(Id("main"),List(),VoidType,Block(List(),List()))))
-  assert(checkAst(input,expected,201))
-}
-  test("another simple program with int as return type of main") {
-    val input = "int main () {}"
-    val expected = Program(List(FuncDecl(Id("main"),List(),IntType,Block(List(),List()))))
+
+  test("001 - Simple var declare") {
+    val input = "int a;"
+    val expected = Program(List(VarDecl(Id("a"),IntType)))
+    assert(checkAst(input,expected,201))
+  }
+
+  test("002 - a simple program with void as return type of main") {
+    val input = "void main () {}"
+    val expected = Program(List(FuncDecl(Id("main"),List(),VoidType,Block(List(),List()))))
     assert(checkAst(input,expected,202))
   }
-  test("a simple program has a simple call putIntLn") {
+  test("003 - another simple program with int as return type of main") {
+    val input = "int main () {}"
+    val expected = Program(List(FuncDecl(Id("main"),List(),IntType,Block(List(),List()))))
+    assert(checkAst(input,expected,203))
+  }
+  test("004 - a simple program has a simple call putIntLn") {
     val input = "void main () {putIntLn(5);}"
     val expected = Program(List(FuncDecl(Id("main"),List(),VoidType,Block(List(),List(CallExpr(Id("putIntLn"),List(IntLiteral(5))))))))
-    assert(checkAst(input,expected,203))
+    assert(checkAst(input,expected,204))
   }
 }
