1a35b35bef63896285bfa5935b77fd1fea00d894
Unknown <thanh.hoquangcse96@gmail.com>
2018-04-19 16:55:57 +0700

    Auto stash before merge of "master" and "origin/master"

--- src/test/scala/AstSuite.scala
+++ src/test/scala/AstSuite.scala
@@ -10,11 +10,13 @@ class AstSuite extends FunSuite with TestAst {
     val expected = Program(List(FuncDecl(Id("main"),List(),VoidType,Block(List(),List()))))
     assert(checkAst(input,expected,201))
   }
+
   test("202 - another simple program with int as return type of main") {
     val input = "int main () {}"
     val expected = Program(List(FuncDecl(Id("main"),List(),IntType,Block(List(),List()))))
     assert(checkAst(input,expected,202))
   }
+
   test("203 - a simple program has a simple call putIntLn") {
     val input = "void main () {putIntLn(5);}"
     val expected = Program(List(FuncDecl(Id("main"),List(),VoidType,Block(List(),List(CallExpr(Id("putIntLn"),List(IntLiteral(5))))))))
@@ -33,7 +35,7 @@ class AstSuite extends FunSuite with TestAst {
     assert(checkAst(input,expected,205))
   }
 
-  test("206 - Simple many var declares with c is Array type") {
+  test("206 - Simple many var declares with c is Array type - type Int") {
     val input = "int a, b, c[1];"
     val expected = Program(List(VarDecl(Id("a"),IntType),VarDecl(Id("b"),IntType),VarDecl(Id("c"),ArrayType(IntLiteral(1),IntType))))
     assert(checkAst(input,expected,206))
@@ -42,6 +44,22 @@ class AstSuite extends FunSuite with TestAst {
   test("207 - Simple many var declares with c is Array type - type String") {
     val input = "string a, b, c[1];"
     val expected = Program(List(VarDecl(Id("a"),StringType),VarDecl(Id("b"),StringType),VarDecl(Id("c"),ArrayType(IntLiteral(1),StringType))))
-    assert(checkAst(input,expected,206))
+    assert(checkAst(input,expected,207))
   }
+
+  test("208 - Simple many var declares with c is Array type - type String + simple Function declare") {
+    val input = """string a, b, c[1];
+int main(){}"""
+    val expected = Program(List(VarDecl(Id("a"),StringType),VarDecl(Id("b"),StringType),VarDecl(Id("c"),ArrayType(IntLiteral(1),StringType)),FuncDecl(Id("main"),List(),IntType,Block(List(),List()))))
+    assert(checkAst(input,expected,208))
+  }
+
+  test("209 - Simple many var declares with c is Array type - type String + 2 simple Function declares") {
+    val input = """string a, b, c[1];
+int main(){}
+string foo(){}"""
+    val expected = Program(List(VarDecl(Id("a"),StringType),VarDecl(Id("b"),StringType),VarDecl(Id("c"),ArrayType(IntLiteral(1),StringType)),FuncDecl(Id("main"),List(),IntType,Block(List(),List())),FuncDecl(Id("foo"),List(),StringType,Block(List(),List()))))
+    assert(checkAst(input,expected,209))
+  }
+
 }
